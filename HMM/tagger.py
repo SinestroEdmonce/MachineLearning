5d34a3d15d58ee4dafcbd8564c24f0321ce2b2b2
SinestroEdmonce <qiaoyi_yin@foxmail.com>
2020-12-06 00:15:04 -0800

    Update tagger.py

--- HMM/tagger.py
+++ HMM/tagger.py
@@ -1,5 +1,7 @@
 import numpy as np
 from hmm import HMM
+from collections import defaultdict
+
 
 def model_training(train_data, tags):
     """
@@ -21,26 +23,48 @@ def model_training(train_data, tags):
     word2idx = {}
     tag2idx = dict()
     S = len(tags)
-    ###################################################
+
     # TODO: build two dictionaries
     #   - from a word to its index 
     #   - from a tag to its index 
     # The order you index the word/tag does not matter, 
     # as long as the indices are 0, 1, 2, ...
-    ###################################################
-    
+    for idx, word in enumerate(word2idx.keys()):
+        word2idx[idx] = word
 
+    for idx, tag in enumerate(tags):
+        tag2idx[tag] = idx
 
     pi = np.zeros(S)
     A = np.zeros((S, S))
     B = np.zeros((S, len(unique_words)))
-    ###################################################
+
     # TODO: estimate pi, A, B from the training data.
     #   When estimating the entries of A and B, if  
     #   "divided by zero" is encountered, set the entry 
     #   to be zero.
-    ###################################################
-    
+    tag2word, tag2tag = defaultdict(lambda: defaultdict(int))
+    initial, transition, tag_counter = defaultdict(int), defaultdict(int), defaultdict(int)
+    for sentence in train_data:
+        prev = None
+        initial[sentence.tags[0]] += 1
+        for i, word in enumerate(sentence.words):
+            tag = sentence.tags[i]
+            tag_counter[tag] += 1
+            tag2word[tag][word] += 1
+            tag2tag[prev][tag] += 1
+            transition[prev] += 1
+            prev = tag
+
+    # Compute the parameters
+    for tag in tags:
+        pi[tag2idx[tag]] = initial[tag] / len(train_data)
+
+        for next in tags:
+            A[tag2idx[tag], tag2idx[next]] = (tag2tag[tag][next] / transition[tag]) if transition[tag] != 0 else 0
+
+        for word, count in tag2word[tag].items():
+            B[tag2idx[tag], word2idx[word]] = (count / tag_counter[tag]) if tag_counter[tag] != 0 else 0
 
     # DO NOT MODIFY BELOW
     model = HMM(pi, A, B, word2idx, tag2idx)
@@ -58,14 +82,25 @@ def sentence_tagging(test_data, model, tags):
     - tagging: (num_sentence*num_tagging) a 2D list of output tagging for each sentences on test_data
     """
     tagging = []
-    ######################################################################
     # TODO: for each sentence, find its tagging using Viterbi algorithm.
     #    Note that when encountering an unseen word not in the HMM model,
     #    you need to add this word to model.obs_dict, and expand model.B
     #    accordingly with value 1e-6.
-    ######################################################################
+    S, index, expanded = len(tags), len(model.obs_dict), 0
+    for sentence in test_data:
+        for word in sentence.words:
+            if word not in model.obs_dict:
+                model.obs_dict[word] = index
+                index += 1
+                expanded += 1
+
+    model.B = np.column_stack(model.B, np.full((S, expanded), 1e-6))
+
+    for sentence in test_data:
+        tagging.append(model.viterbi(Osequence=sentence.words))
 
     return tagging
+
 
 # DO NOT MODIFY BELOW
 def get_unique_words(data):
